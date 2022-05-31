ad1517b0c919f068168394bcdfbb09d2b550e8a7
Oksana Semko <osemko@gmail.com>
2022-05-31 03:21:11 +0300

    task_02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
 
+const KEY = 'videoplayer-current-time';
+
+const player = new Player('vimeo-player');
+const key = localStorage.getItem(KEY);
+if (key) {
+  player.setCurrentTime(parcelFloat(key));
+}
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(KEY, data.seconds.toString());
+  }, 1000)
+);
